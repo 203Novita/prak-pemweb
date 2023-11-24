f5358dbefafb5caa44b78e697f2682ab88521860
203Novita <116406235+203Novita@users.noreply.github.com>
2023-11-24 12:37:45 +0700

    Update 121140203_Novita Rahmadhani_Prak7.php

--- Prak7/121140203_Novita Rahmadhani_Prak 7.php
+++ Prak7/121140203_Novita Rahmadhani_Prak 7.php
@@ -1,1 +1,55 @@
+<!-- by Novita Rahmadhani - 121140203-->
+<!-- Pemrograman Web RB - Praktikum 7 PHP-->
 
+<?php
+
+class Mahasiswa {
+    private $nama;
+    private $nim;
+    private $prodi;
+
+    public function setNama($nama) {
+        $this->nama = $nama;
+    }
+
+    public function getNama() {
+        return $this->nama;
+    }
+
+    public function setNim($nim) {
+        $this->nim = $nim;
+    }
+
+    public function getNim() {
+        return $this->nim;
+    }
+
+    public function setProdi($prodi) {
+        $this->prodi = $prodi;
+    }
+
+    public function getProdi() {
+        return $this->prodi;
+    }
+
+    public function display() {
+        echo "Nama  : " . $this->getNama() . "<br>";
+        echo "NIM   : " . $this->getNim() . "<br>";
+        echo "Prodi : " . $this->getProdi() . "<br>";
+        echo "<br>";
+    }
+}
+
+$mahasiswa1 = new Mahasiswa();
+$mahasiswa1->setNama("Zeddhy Recca Fitracia");
+$mahasiswa1->setNim("121140199");
+$mahasiswa1->setProdi("Teknik Informatika");
+$mahasiswa1->display();
+
+$mahasiswa2 = new Mahasiswa();
+$mahasiswa2->setNama("Novita Rahmadhani");
+$mahasiswa2->setNim("121140105");
+$mahasiswa2->setProdi("Teknik Informatika");
+$mahasiswa2->display();
+
+?>
